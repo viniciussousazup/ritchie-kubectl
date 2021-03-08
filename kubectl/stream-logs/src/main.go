3c59c75b28bf91749b08d8441e644d1da78a0834
viniciusousazup <vinicius.sousa@zup.com.br>
2021-03-08 18:05:10 -0300

    add input

--- kubectl/stream-logs/src/main.go
+++ kubectl/stream-logs/src/main.go
@@ -6,21 +6,33 @@ package main
 import (
 	"formula/pkg/formula"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 
+	podLogLevel := os.Getenv("POD_LOG_LEVEL")
+
+	logLevels := strings.Split(podLogLevel, "|")
+
+	logLevel := map[string]bool{
+		"info":  false,
+		"error": false,
+		"panic": false,
+		"fatal": false,
+		"warn":  false,
+		"debug": false,
+	}
+
+	for _, log := range logLevels {
+		logLevel[log] = true
+	}
+
 	formula.Formula{
 		LabelSelector: "app.kubernetes.io/instance in (dennis-runner, dennis-builder-wrapper, dennis-gateway)",
 		SinceTime:     metav1.NewTime(time.Now().Add(-1 * time.Duration(0))),
-		LogLevel: map[string]bool{
-			"info":  true,
-			"error": true,
-			"panic": true,
-			"fatal": true,
-			"warn":  false,
-			"debug": false,
-		},
+		LogLevel:      logLevel,
 	}.Run()
 }
